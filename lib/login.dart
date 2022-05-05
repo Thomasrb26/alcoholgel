579e6684024ab52e478c3a7c07170f5b232fbc3b
Thomasrb26 <51177606+Thomasrb26@users.noreply.github.com>
2022-05-04 21:45:22 -0400

    first commit

---
+++ lib/login.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  static String id = 'login_page';
+
+  //LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginpageState createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              'images/logoingenieria.png',
+              height: 100,
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key); //constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: LoginPage.id,
+      routes: {
+        LoginPage.id: (context) => LoginPage(),
+      },
+    );
+  }
+}
