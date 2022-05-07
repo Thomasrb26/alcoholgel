c1c714f9cbb5d19f3ac4af574dac6438b77e92bc
Camilo <camilo.svs@gmail.com>
2022-05-06 20:39:53 -0400

    Avance Alertas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/login.dart';
+import 'package:flutter_application_1/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
