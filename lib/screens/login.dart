96821f04f54c77d05b13fe61e6ccc084015849f6
thomas <tomas_hormazabal@hotmail.com>
2022-05-10 23:42:51 -0400

    avance vista simple login. falta colores

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -14,17 +14,61 @@ class _LoginpageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              Image.asset(
+          body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Flexible(
+              child: Image.asset(
                 'images/logoingenieria.png',
                 height: 100,
-              )
-            ],
-          ),
-        )
-      ),
+              ),
+            ),
+            const SizedBox(
+              height: 15.0,
+            ),
+            solicitarRut(),
+            const Spacer(),
+            _botonlogin(),
+          ],
+        ),
+      )),
     );
   }
+
+  Widget solicitarRut() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: TextField(
+          keyboardType: TextInputType.url,
+          decoration: const InputDecoration(
+              icon: Icon(Icons.login),
+              hintText: '12345678-9',
+              labelText: ('Ingrese su rut')),
+          onChanged: (value) {},
+        ),
+      );
+    });
+  }
+}
+
+Widget _botonlogin() {
+  return StreamBuilder(builder: (BuildContext context, AsyncSnapshot snapshot) {
+    return RaisedButton(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+          child: const Text('Entrar',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              )),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        elevation: 20,
+        onPressed: () {});
+  });
 }
--- lib/screens/usuario/HomeAlertasScreen.dart
+++ lib/screens/usuario/HomeAlertasScreen.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 
 class HomeAlertasScreen extends StatelessWidget {
-   
   const HomeAlertasScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-         child: Text('HomeAlertasScreen'),
+        child: Text('HomeAlertasScreen'),
       ),
     );
   }
