8abfdc233cde8ae6a8fb22e00c6f7b1c48eb3cd3
thomas <tomas_hormazabal@hotmail.com>
2022-05-19 08:46:13 -0400

    login funcional

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screens/login.dart';
-import 'package:flutter_application_1/screens/usuario/ScannerQr.dart';
-import 'package:flutter_application_1/screens/usuario/usuario.dart';
-import 'package:flutter_application_1/services/services.dart';
-import 'package:flutter_application_1/themes/app_theme.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_application_1/import.dart';
 
 void main() {
   runApp(const AppState());
@@ -34,9 +29,11 @@ class MyApp extends StatelessWidget {
       initialRoute: 'login',
       routes: {
         'login': (context) => const LoginPage(),
-        'home':(context) => const MisAlertasScreen(),
+        'vista_alertas': (context) => const MisAlertasScreen(),
         // 'scanqr':(context) => const QrScan(),
-        'alertaInfo': (context) => const AlertaInfoScreen()
+        'alertaInfo': (context) => const AlertaInfoScreen(),
+        'loginUsuario': ((context) => const LoginUsuario()),
+        'loginFun': ((context) => const LoginFuncionario()),
       },
     );
   }
--- lib/screens/usuario/usuario.dart
+++ lib/screens/usuario/usuario.dart
@@ -1,4 +1,5 @@
 export 'package:flutter_application_1/screens/usuario/alerta_info.dart';
 export 'package:flutter_application_1/screens/usuario/mis_alertas_screen.dart';
-
-
+export 'package:flutter_application_1/screens/usuario/diseno_login.dart';
+export 'package:flutter_application_1/screens/usuario/card_login.dart';
+export 'package:flutter_application_1/screens/usuario/input_decorations.dart';
